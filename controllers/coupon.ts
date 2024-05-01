80dc6f3aca8b60377b23f7480999b5e9b389aa23
boodyshlby55 <boodyshlby55@gmail.com>
2024-05-01 23:56:50 +0300

    update coupons & carts

--- controllers/coupon.ts
+++ controllers/coupon.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import couponModel from "../models/couponModel";
-import { CouponModel } from "../interfaces";
+import { CouponModel, FilterData } from "../interfaces";
 import { createOne, deleteOne, getAll, getOne, updateOne } from "./refactorHandler";
 
-const getCoupons = getAll<CouponModel>(couponModel, 'coupon');
+const getCoupons = getAll<CouponModel>(couponModel, 'coupons');
 const createCoupon = createOne<CouponModel>(couponModel);
-const getCoupon = getOne<CouponModel>(couponModel);
+const getCoupon = getOne<CouponModel>(couponModel, 'coupons');
 const updateCoupon = updateOne<CouponModel>(couponModel);
 const DeleteCoupon = deleteOne<CouponModel>(couponModel);
 
-export { getCoupons, createCoupon, getCoupon, updateCoupon, DeleteCoupon };
+const filterCoupons = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+    // ! important update after e-commerce frontend
+    let filterData: FilterData = {};
+    if (req.user?.role !== 'customer') { filterData.shop = req.user?.shop; };
+    req.filterData = filterData;
+    next();
+};
+
+export { getCoupons, createCoupon, getCoupon, updateCoupon, DeleteCoupon, filterCoupons };
--- models/couponModel.ts
+++ models/couponModel.ts
@@ -1,21 +1,25 @@
 import mongoose from "mongoose";
 import { CouponModel } from "../interfaces";
-const couponSchema: mongoose.Schema = new mongoose.Schema < CouponModel >({
-    name:{
-        type:String,
-        trim:true,
-        required: [true,"coupon name is required"],
+const couponSchema: mongoose.Schema = new mongoose.Schema<CouponModel>({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, "coupon name is required"],
         unique: true,
     },
-    expire:{
+    expire: {
         type: Date,
-        required:[true," coupon expire time is required"],
+        required: [true, "coupon expire time is required"],
     },
-    discount:{
-        type:Number,
-        required:[true,"coupon discount is required"],
+    discount: {
+        type: Number,
+        required: [true, "coupon discount is required"],
+    },
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "shops"
     },
 },
     { timestamps: true }
 );
-export default mongoose.model < CouponModel > ("coupon", couponSchema);
+export default mongoose.model<CouponModel>("coupons", couponSchema);
