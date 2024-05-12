1aa15d8de56c7e0324f514fb77914b3c83d2d52b
ayaatea <aaatea03@gmail.com>
2024-05-12 03:03:31 +0300

    create get routes&update order status

--- routes/orderRoute.ts
+++ routes/orderRoute.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { allowedTo, checkActive, protectRoutes } from "../controllers/auth";
-import { createCashOrder } from "../controllers/order"
+import { createCashOrder,getAllOrders,getSecificOrder,filterObjectForLoggedUser,updateOrederToPaid,updateOrederToDelivered } from "../controllers/order"
 const orderRoute = Router();
-orderRoute.use(protectRoutes, checkActive, allowedTo('customer'));
+orderRoute.use(protectRoutes, checkActive);
 
-orderRoute.route("/:id")
-    .post(createCashOrder)
-
+orderRoute.route("/:id").post(allowedTo('customer'),createCashOrder)
+    orderRoute.route("/").get(allowedTo('customer','user','admin'),filterObjectForLoggedUser,getAllOrders)
+    orderRoute.route("/:id").get(getSecificOrder)
+    orderRoute.route("/:id/pay").put(allowedTo('user','admin'),updateOrederToPaid)
+    orderRoute.route("/:id/deliver").put(allowedTo('user','admin'),updateOrederToDelivered)
 
 
 export default orderRoute;
