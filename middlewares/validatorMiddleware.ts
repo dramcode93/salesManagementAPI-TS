4d326a0bbbdd602821e78b567ac8ba96af8df3f4
boodyshlby55 <boodyshlby55@gmail.com>
2024-06-04 05:26:52 +0300

    class validator

--- middlewares/validatorMiddleware.ts
+++ middlewares/validatorMiddleware.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { validationResult } from "express-validator";
 
-const validatorMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+const validatorMiddleware: express.RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
     else { next(); };
